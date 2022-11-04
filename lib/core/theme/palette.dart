04b6610f44ed141999ceddfccf726c62b52a0371
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-11-04 12:41:55 +0100

    add default radius and padding

--- lib/core/theme/palette.dart
+++ lib/core/theme/palette.dart
@@ -4,8 +4,31 @@ import 'package:flutter/material.dart';
 
 /// Color palette
 enum ColorPalette {
-  primary(Colors.indigoAccent);
+  primary(Color(0xFF7968F8)),
+  secondary(Color.fromARGB(255, 191, 131, 255)),
+  grey(Color(0xFFB8C4E2)),
+  background(Colors.white),
+  text(Color(0xFF303E65)),
+  red(Color(0xFFFA5A7E));
 
   final Color color;
   const ColorPalette(this.color);
+}
+
+enum ThemePadding {
+  small(10),
+  medium(20),
+  large(30);
+
+  final double padding;
+  const ThemePadding(this.padding);
+}
+
+enum ThemeRadius {
+  small(5),
+  medium(10),
+  large(20);
+
+  final double radius;
+  const ThemeRadius(this.radius);
 }
